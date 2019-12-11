7ca86302a668d49935db02df786c678b1e0adca8
jonathan Cuendet <jonathan.cuendet@mindera.com>
2019-12-11 15:36:04 +0000

    updates

---
+++ AcademiaCodigoUITesting/Service/KittenService.swift
@@ -0,0 +1,9 @@
+//
+//  KittenService.swift
+//  AcademiaCodigoUITesting
+//
+//  Created by jonathan Cuendet on 11/12/2019.
+//  Copyright © 2019 jonathan Cuendet. All rights reserved.
+//
+
+import Foundation
---
+++ AcademiaCodigoUITesting/ViewControllers/CollectionScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CollectionScreenViewController.swift
+//  AcademiaCodigoUITesting
+//
+//  Created by jonathan Cuendet on 11/12/2019.
+//  Copyright © 2019 jonathan Cuendet. All rights reserved.
+//
+
+import UIKit
+
+class CollectionScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
